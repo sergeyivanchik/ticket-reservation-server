185536726db25b6f463e7cfa26f4ac29572e9322
sergeyivanchik <ivanchik-s@mail.ru>
2019-03-19 23:01:33 +0300

    server work with cards description

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 var jsonParser = bodyParser.json();
  
 // получение списка данных
-app.get("/api/users", function(req, res){
+app.get("/api/cards", function(req, res){
       
-    var content = fs.readFileSync("users.json", "utf8");
-    var users = JSON.parse(content);
-    res.send(users);
+    var content = fs.readFileSync("cardDescription.json", "utf8");
+    var cards = JSON.parse(content);
+    res.send(cards);
 });
 app.listen(8080, function(){
   console.log("Сервер ожидает подключения...");
