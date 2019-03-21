f68367af0907c78be816f970a6ceed645e3a4d6f
sergeyivanchik <ivanchik-s@mail.ru>
2019-03-21 15:54:37 +0300

    added check for the number of films in db

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 const filmModel = require("../models/film.js");
 
-router.get("/", function(req, res){      
+router.get("/", function(req, res){ 
+filmModel.countDocuments( function(err,count) {
+  if(err) return console.log(err);
+  if (count >=6) {
     filmModel.find({}, function(err, films){
       if(err) return console.log(err);
       res.json(films)
-    }).sort({_id:-1}).limit(6);
-  });
+    })
+  }
+  else {
+    res.json('GG');
+  }});
+});
 
-  module.exports = router;
+module.exports = router;
