18ddf7f76e583b47166d2ba14913506852ca8126
ak1moradev <ak1mora@yandex.com>
2019-03-28 23:31:45 +0300

    Rewrite with async/await

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
 const filmModel = require("../models/film.js");
 
-router.get("/", function(req, res){ 
-    filmModel.find({}, function(err, films){
-      if(err) return console.log(err);
-      res.json(films)
-    }).sort({_id:-1}).limit(6);
+// функциональный стиль + async/await на промисах
+router.get("/", async (req, res) => { 
+  try {
+    const films = await filmModel.find({}).sort({_id:-1}).limit(6)
+    /**
+     * В REST API лучше возращать объекты, которые описывают ответ, а не их значения, чтобы на клиенте было проще.
+     * Это вроде не стандарт и нигде о таком не написано, но вроде реально проще
+     */
+    return res.json({ films })
+  } catch (error) {
+    return res.json({
+      error: error.message
+    })
+  }
 });
 
 module.exports = router;
