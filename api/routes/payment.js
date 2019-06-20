8a8394b4321e0d100b45626e50ee3265805eab9a
sergeyivanchik <ivanchik-s@mail.ru>
2019-06-20 19:34:13 +0300

    add payment

---
+++ api/routes/payment.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const stripeKey = require('../configs/payment').stripeKey
+const stripe = require("stripe")(stripeKey);
+
+
+
+router.post("/", async (req, res) => {
+  try {
+    let {status} = await stripe.charges.create({
+      amount: req.body.cost * 100,
+      currency: "usd",
+      description: "Charge for tickets",
+      source: req.body.token.id
+    });
+    res.send({status});
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
+module.exports = router;
